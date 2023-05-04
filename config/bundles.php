bf8135138c4185170414e1355ef96c6206ab24cb
Pascal Pitette <ppitette@gmail.com>
2023-05-04 11:17:34 +0200

    feat: reconfiguration

--- config/bundles.php
+++ config/bundles.php
@@ -12,8 +12,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -61,9 +61,8 @@ Encore
 
     // enables Sass/SCSS support
     .enableSassLoader()
-
     .enablePostCssLoader()
-
+    
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
